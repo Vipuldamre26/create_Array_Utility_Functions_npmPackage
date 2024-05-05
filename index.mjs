1f6fb3218e2203e17e19af3fdba01aaec6a70757
Vipul <vipuldamrepro26@gmail.com>
2024-05-05 21:39:59 +0530

    modify indexmjs

--- index.mjs
+++ index.mjs
@@ -64,7 +64,7 @@ function removeDuplicateElements(arr){
 
 
 
-export default { sumOfArray, multipleOfArray, sortArrayByAccending, sortArrayByDecending, binarySearch, largestNumInArray, removeDuplicateElements }
+export { sumOfArray, multipleOfArray, sortArrayByAccending, sortArrayByDecending, binarySearch, largestNumInArray, removeDuplicateElements }
 
 
 // let arr = [4, 3, 1, 8, 9, 2, 4, 1, 8, 4, 4, 3]
